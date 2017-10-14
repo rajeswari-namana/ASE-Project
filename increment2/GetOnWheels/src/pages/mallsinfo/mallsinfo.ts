f5307f77d382020d2c0cacb5bd17c55d35dd6355
Sravanthi-Gogadi <sravanthigogadi@gmail.com>
2017-10-13 20:33:46 -0500

    Project

---
+++ increment2/GetOnWheels/src/pages/mallsinfo/mallsinfo.ts
@@ -0,0 +1,39 @@
+import { Component ,ViewChild ,ElementRef } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import {MallsdetailsProvider} from "../../providers/mallsdetails/mallsdetails";
+
+/**
+ * Generated class for the MallsinfoPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+
+@Component({
+  selector: 'page-mallsinfo',
+  templateUrl: 'mallsinfo.html',
+})
+export class MallsinfoPage {
+  mallsinfo: any;
+  mallsData: any={};
+  pic: any={};
+  rev: any={};
+  constructor(public navCtrl: NavController, public navParams: NavParams, public malls: MallsdetailsProvider) {
+    this.mallsinfo=navParams.get('mallsinfo')
+    this.malls.detailedMalls(this.mallsinfo)
+      .then(data => {
+        this.mallsData = data;
+        this.pic= this.mallsData.photos[0];
+        this.rev= this.mallsData.reviews[1];
+        console.log(this.mallsData.rating);
+      })
+
+
+  }
+
+
+  getArray(size): Array<any> {
+    return new Array(size);
+  }
+}
